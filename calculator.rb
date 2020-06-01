b9050d020b8f22ee0c6472d4505a16841e5e2e9d
Tracy Collins <tc@threeceemedia.com>
2020-06-01 19:44:19 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 47
+second_number = 147
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
